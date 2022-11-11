fb86319a02551a93391540e8603ee8adc1624934
r-scott25 <rachelbernal2@gmail.com>
2022-11-11 17:34:08 -0600

    challenge 1 - initials

---
+++ challenges/Initials.java
@@ -0,0 +1,13 @@
+package challenges;
+
+public class Initials {
+  public static void main(String[] args) {
+    System.out.println("RRRR    SSS ");
+    System.out.println("R   R  S   S");
+    System.out.println("R   R  S    ");
+    System.out.println("RRRR    SSS ");
+    System.out.println("R R        S");
+    System.out.println("R  R   S   S");
+    System.out.println("R   R   SSS ");
+  }
+}
