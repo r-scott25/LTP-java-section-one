f0e17171167284fbba6011ef2230f0f676f7db0b
r-scott25 <rachelbernal2@gmail.com>
2022-11-11 18:46:14 -0600

    string and char

---
+++ variables/Form.java
@@ -0,0 +1,8 @@
+package variables;
+
+public class Form {
+  public static void main(String[] args) {
+    char gender = 'F';
+    System.out.println("Gender: " + gender);
+  }
+}
---
+++ variables/JoiningStrings.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class JoiningStrings {
+  public static void main(String[] args) {
+    String sentence = "His name is: ";
+    String placeholder = "Harry";
+    System.out.println(sentence + placeholder);
+
+    int points = 50;
+    String announcement = " points for Gryffindor";
+    System.out.println(points + announcement);
+  }
+}
