029f0da604abbd743a750647e7fad9f63b14ed22
r-scott25 <rachelbernal2@gmail.com>
2022-11-11 18:16:07 -0600

    workbook 2.1 Points

---
+++ variables/Bus.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class Bus {
+  public static void main(String[] args) {
+    // int passengers = 0;
+    // passengers += 9;
+    // System.out.println(passengers);
+    // passengers -= 5;
+    // System.out.println(passengers);
+    // passengers -= 4;
+    // System.out.println(passengers);
+
+    int foo = 2;
+ 
+    int bar = foo - 6;
+    foo += bar;
+
+    System.out.println(foo);
+    System.out.println(bar);
+  }
+}
---
+++ variables/Variables.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class Variables {
+  public static void main(String[] args) {
+    int passengers = 20;
+    System.out.println(passengers);
+
+    int busTickets = passengers;
+    System.out.println(busTickets);
+  }
+}
