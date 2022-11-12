a298129ccc990f01e1f17e2c218790bdd8655ec2
r-scott25 <rachelbernal2@gmail.com>
2022-11-11 19:09:45 -0600

    int and long

---
+++ variables/WholeNumbers.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class WholeNumbers {
+  public static void main(String[] args) {
+    int numberOfFans = 5000;
+    System.out.println("There are " + numberOfFans + " fans at the Quidditch games.");
+
+    long globalPopulation = 7800000000L;
+    System.out.println("There are " + globalPopulation + " people on Earth.");
+
+    long dailyGoogleSearches = 5000000000L;
+    System.out.println("Daily, there are " + dailyGoogleSearches + " Google searches.");
+  }
+}
